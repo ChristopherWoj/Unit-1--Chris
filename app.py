0f7423facc63fde8d4a0875c2002c74af18cbf91
ChristopherWoj <christopherw334@nycstudents.net>
2026-09-14 10:21:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+
+t.shape('turtle')
+def square(x):
+    t.forward(x)
+    t.left(180)
+    t.forward(x)
+    t.left(180)
+    t.forward(x)
+    t.left(180)
+    t.forward(x)
+    t.left(180)
+square(100)
+
+
+
+turtle.done()
+
+def message(input):
+    print(input)
+message("Hello Class")
+
